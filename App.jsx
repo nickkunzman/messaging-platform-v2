a51633b4adf296acb25f41e00a8c479803abba64
nickkunzman <nick@natalierobersonphotography.com>
2025-05-07 13:00:28 -0500

    Update App.jsx

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 40, fontSize: "24px", color: "green" }}>
+      ✅ React is working!
+    </div>
+  );
+}
