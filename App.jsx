ffe6d73c1d4ad65c730cd69e5ce3a4210e03c357
nickkunzman <nick@natalierobersonphotography.com>
2025-05-07 13:17:01 -0500

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function App() {
-  return (
-    <div style={{ padding: 40, fontSize: "24px", color: "green" }}>
-      ✅ React is working!
-    </div>
-  );
-}
